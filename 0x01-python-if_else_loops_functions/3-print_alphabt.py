0320f93b8992d922456506f28df2f49b7eab18fa
Isaacdvs2 <dvs2mona@gmail.com>
2022-11-30 08:29:52 +0000

    debugged script for pycodestyle errors

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 for elements in range(97, 123):
-	if elements != 101 and elements != 113:
-		print("{}".format(chr(elements)),end="")
+ if elements != 101 and elements != 113:
+  print("{}".format(chr(elements)),end="")
