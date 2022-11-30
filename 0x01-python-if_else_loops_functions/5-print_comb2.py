6ad94a24ed15565397bddec2b8f9ae19e0b9ca9f
Isaacdvs2 <dvs2mona@gmail.com>
2022-11-30 17:03:01 +0000

    add print comb script

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-for (i,j) in [(i,j) for i in range(0, 10) for j in range(0, 10)]:
-        if (i + j == 18):
-            print("{}{} ".format(i,j))
-        else:
-            print("{}{}, ".format(i, j), end = "")
+for i in range(0, 99):
+    print("{:02d}, ".format(i), end = "")
+print("{:02d}".format(i + 1))
