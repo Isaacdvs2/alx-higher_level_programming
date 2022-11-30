df10f0cd7739eb7ae10944f653c0130a880dc1fd
Isaacdvs2 <dvs2mona@gmail.com>
2022-11-30 16:08:27 +0000

    fixed script output errors

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(0, 10):
-    for j in range(0, 10):
+for (i,j) in [(i,j) for i in range(0, 10) for j in range(0, 10)]:
         if (i + j == 18):
-            print("{}{} ".format(i,j), end = "")
+            print("{}{} ".format(i,j))
         else:
             print("{}{}, ".format(i, j), end = "")
