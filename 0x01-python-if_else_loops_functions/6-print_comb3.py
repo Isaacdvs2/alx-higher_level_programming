c8bd65b924366b9d213fbaa2fc55a713cfc3b504
Isaacdvs2 <dvs2mona@gmail.com>
2022-12-01 01:29:40 +0000

    debugged script for errors

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(0, 10):
-    for j in range (0, 10):
-       if j > i:
-            print("{}{}".format(i, j), end = ", ")
-       elif i + j == 17:
-            print("{}{}".format(i, j))
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        print("{}{}".format(i, j), end=', ')
+print("{}{}".format(i + 1, j))
+
