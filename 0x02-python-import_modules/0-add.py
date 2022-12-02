d901dce24ca254c5c569e491214b6ff1f07fef3f
Isaacdvs2 <dvs2mona@gmail.com>
2022-12-02 09:20:50 +0000

    debugged file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 import sys
-from add_0 import add
-a = 1
-b = 2
 
 if __name__ == "__main__":
+
+    from add_0 import add
+    a = 1
+    b = 2
+
     print("{} + {} = {}".format(a, b, add(a, b)))
+    
     sys.exit(0)
