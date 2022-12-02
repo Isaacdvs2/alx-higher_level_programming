fbe64ceeff065bd134e85dd750c50419f0773d0a
Isaacdvs2 <dvs2mona@gmail.com>
2022-12-02 12:11:49 +0000

    removed sys import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-import sys
-
 if __name__ == "__main__":
 
     from add_0 import add
@@ -9,5 +7,3 @@ if __name__ == "__main__":
     b = 2
 
     print("{} + {} = {}".format(a, b, add(a, b)))
-    
-    sys.exit(0)
