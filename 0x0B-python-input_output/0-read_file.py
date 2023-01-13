45f30c9faee4079f523757700b8202d867be3112
Isaacdvs2 <dvs2mona@gmail.com>
2023-01-13 09:48:07 +0000

    edited read method

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(file_name=""):
     """ A method that takes the filename as input and displays it
     to std out """
     with open(file_name, encoding = "utf-8") as f:
-        print(f.readlines(), end = "")
+        print(f.read(), end = "")
