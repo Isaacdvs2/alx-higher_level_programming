59765ac4df412966767182c8ed624bae9089bfdd
Isaacdvs2 <dvs2mona@gmail.com>
2023-02-16 11:27:24 -0800

    debugged script

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,5 @@
--- a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP by CITY ORDER BY avg_temp DESC;
+-- convert hbtn_0c_0 db to utf8
+
+ALTER TABLE `hbtn_0c_0`.`first_table` MODIFY `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `hbtn_0c_0`.`first_table` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
