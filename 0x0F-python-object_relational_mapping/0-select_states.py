5142aabf0dea3131c0afd116176ffbdaeab5d024
Isaacdvs2 <dvs2mona@gmail.com>
2023-03-28 06:02:49 -0700

    added port num

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,9 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], host='localhost')
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], host='localhost', port=3306)
     crs = db.cursor()
     crs.execute("SELECT * FROM `states`")
-    [print(state) for state in crs.fetchall()]
+    
+    for state in crs.fetchall():
+        print(state)
